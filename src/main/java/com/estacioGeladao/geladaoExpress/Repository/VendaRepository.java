e0d70322df91e6ab70b49e6a31b32460ee579e5d
Victor Facundo <victorfacundo122@hotmail.com>
2024-03-28 13:03:15 -0300

    Atualizando banco para postgre, criando service e repository Venda

---
+++ src/main/java/com/estacioGeladao/geladaoExpress/Repository/VendaRepository.java
@@ -0,0 +1,9 @@
+package com.estacioGeladao.geladaoExpress.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.estacioGeladao.geladaoExpress.entities.Venda;
+
+public interface VendaRepository extends JpaRepository<Venda, Long> {
+
+}
---
+++ src/main/java/com/estacioGeladao/geladaoExpress/Service/VendaService.java
@@ -0,0 +1,17 @@
+package com.estacioGeladao.geladaoExpress.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.estacioGeladao.geladaoExpress.Repository.VendaRepository;
+
+@Service
+public class VendaService {
+
+	@Autowired
+	private VendaRepository vendaRepository;
+	
+	
+	
+	
+}
